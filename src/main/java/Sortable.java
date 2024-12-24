789228bf96c8618d68e367a59303932e364995f2
A A <a@a>
2024-12-24 12:04:33 +0300

    Sortable ShellSort

--- src/main/java/Sortable.java
+++ src/main/java/Sortable.java
@@ -1,8 +1,2 @@
 public interface Sortable<T> {
-    void sortByFirstField(); // сортировка по первому полю (Мощность, автор, тип)
-
-    void sortBySecondField(); // сортировка по второму полю (модель, название, вес)
-
-    void sortByThirdField(); // сортировка по третьему полю (год производства, количество страниц, цвет)
-
 }
