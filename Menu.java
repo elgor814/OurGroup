342fea13777a4ee82f3baf9f7bb7c55d1c92dd4f
Влад <107302705+XT7ZY@users.noreply.github.com>
2024-12-26 00:54:56 +0300

    Add base menu

---
+++ Menu.java
@@ -0,0 +1,13 @@
+public class Menu {
+
+    public static void showStartMenu(){
+        System.out.println("Выберите действие:");
+        System.out.println("1. Ввести данные вручную");
+        System.out.println("2. Сгенерировать данные случайны образом");
+        System.out.println("3. Загрузить данные из файла");
+        System.out.println("4. Отсортировать данные");
+        System.out.println("5. Найти элемент с помощью бинарного поиска");
+        System.out.println("6. Выход");
+    }
+
+}
