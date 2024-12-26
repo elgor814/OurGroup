bb740827e62a6c03d85c6207d14df29436ec86ff
VeraKalinkin <153802442+VeraKalinkin@users.noreply.github.com>
2024-12-26 15:00:49 +0400

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,20 +1,49 @@
-package third_task;
+import java.util.ArrayList;
 
-public class BinarySearch {
-    public static <T extends Comparable<T>> int binarySearch(T[] array, T key) {
+public interface BinarySearch {
+
+    static <T extends Comparable<T>> void binarySearch(ArrayList<Object> array, T key) {
+
+        int low = 0;
+        int high = array.size() - 1;
+        boolean flag = false;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (array.get(mid).equals(key)){
+                System.out.println(array.get(mid));
+                flag = true;
+                break;
+            }
+
+            else if (((T) array.get(mid)).compareTo(key) > 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        if (!flag)
+            System.out.println("Объект не найден");
+    }
+
+    static <T extends Comparable<T>> void binarySearch(T[] array, T key) {
         int low = 0;
         int high = array.length - 1;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
             int comparison = array[mid].compareTo(key);
-            if (comparison == 0) return mid;
+            if (comparison == 0)
+                System.out.println(array[mid]);
             if (comparison < 0) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
         }
-        return -1;
+        System.out.println("Объекст не найден");
     }
+
 }
