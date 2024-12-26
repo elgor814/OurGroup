a055b4d6cf44589e62faae9f669dfb0ef4588bcc
elgor814 <168122154+elgor814@users.noreply.github.com>
2024-12-26 17:56:23 +0300

    Update ShellSort.java

--- ShellSort.java
+++ ShellSort.java
@@ -1,13 +1,31 @@
-package main.java;
+import java.util.*;
 
-
-import java.util.Collection;
-
-public class ShellSort<T extends Comparable<T>> implements ISortStrategy<T> {
-
+public class ShellSort<T> implements ISortStrategy<T> {
 
     @Override
-    public void sort(Collection<T> collection) {
-        //some sort
+    public List<T> sort(Collection<T> collection, Comparator<T> comparator) {
+
+        // Преобразуем коллекцию в массив
+        T[] array = (T[]) collection.toArray();
+
+        int n = array.length;
+
+        // Применяем сортировку Шелла
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i++) {
+                T temp = array[i];
+                int j = i;
+                while (j >= gap && comparator.compare(array[j - gap], temp) > 0) {
+                    array[j] = array[j - gap];
+                    j -= gap;
+                }
+                array[j] = temp;
+            }
+        }
+
+        // Обновляем коллекцию из отсортированного массива
+        return new ArrayList<>(Arrays.asList(array));
     }
 }
+
+
